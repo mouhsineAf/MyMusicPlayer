125f8e826ee9baa1882300c6b8a1d4768b47436a
mouhsine <mouhsinem10000@gmail.com>
2022-01-27 09:23:50 +0100

    new

--- app/src/main/java/com/my/musicplayer/activity/MainActivity.java
+++ app/src/main/java/com/my/musicplayer/activity/MainActivity.java
@@ -2,12 +2,17 @@ package com.my.musicplayer.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.my.musicplayer.R;
 import com.my.musicplayer.fragment.HomeFragment;
 import com.my.musicplayer.fragment.SettingsFragment;
@@ -15,6 +20,10 @@ import com.my.musicplayer.fragment.SongsFragment;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +38,34 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new HomeFragment()).commit();
         }
-        
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull  MenuItem item) {
+
+                int id = item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id)
+                {
+
+                    case R.id.nav_home:
+                        Toast.makeText(MainActivity.this, "Home is Clicked", Toast.LENGTH_SHORT).show();break;
+                    case R.id.nav_songs:
+                        Toast.makeText(MainActivity.this, "Message is Clicked",Toast.LENGTH_SHORT).show();break;
+                    case R.id.nav_settings:
+                        Toast.makeText(MainActivity.this, "Synch is Clicked",Toast.LENGTH_SHORT).show();break;
+
+                    default:
+                        return true;
+
+                }
+                return true;
+            }
+        });
+
+        setToolBar();
         
     }
 
@@ -58,6 +94,13 @@ public class MainActivity extends AppCompatActivity {
                 }
 
     };
+
+    private void setToolBar(){
+        ImageView imageDrawer = findViewById(R.id.btn_drawer);
+        imageDrawer.setOnClickListener(view -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+    }
 
 
 }
