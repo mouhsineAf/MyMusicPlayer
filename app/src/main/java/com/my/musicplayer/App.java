0b8676ef851f938a68f365ff78f36606dcf7f4f6
mouhsine <mouhsinem10000@gmail.com>
2022-01-27 01:26:42 +0100

    new

---
+++ app/src/main/java/com/my/musicplayer/App.java
@@ -0,0 +1,16 @@
+package com.my.musicplayer;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/my/musicplayer/Config.java
@@ -0,0 +1,4 @@
+package com.my.musicplayer;
+
+public class Config {
+}
